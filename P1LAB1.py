2a111709147c21e4b60603260b8d5cfcab12913a
Tdba1873 <barrettt9230@student.faytechcc.edu>
2024-06-20 13:45:19 -0400

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,17 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: C:/Users/barrettt9230/zzzzz.py ===================
+>>> 
+==================== RESTART: C:/Users/barrettt9230/zzzzz.py ===================
+ Enter your first name: Travis
+ Enter your last name: Barrett
+ Hello, Travis Barrett!  Welcome to CTI-110
+==================== RESTART: C:/Users/barrettt9230/zzzzz.py ===================
+ Enter your first name: Travis
+ Enter your last name: Barrett
+ Hello, Travis Barrett!  Welcome to CTI-110
+==================== RESTART: C:/Users/barrettt9230/zzzzz.py ===================
+Enter your first name: Travis
+Enter your last name: Barrett
+Hello, Travis Barrett! Welcome to CTI-110
