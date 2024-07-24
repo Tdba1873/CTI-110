3a4d90fa32191f4a87e86b3528c0c5ca52a50de3
Tdba1873 <barrettt9230@student.faytechcc.edu>
2024-07-23 21:24:19 -0400

    Add files via upload

---
+++ P1HW1_BarrettTravis.py
@@ -0,0 +1,81 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: F:\P1HW1_BarrettTravis.py
+
+
+============================= RESTART: F:\P1HW1.py =============================
+----- Calculating Exponenets-----
+
+
+
+
+Enter an integer as the base value:
+Enter an integer as the exponent:
+
+
+
+8 raised to the power of 4 is 4096 !!
+
+
+
+----- Addition and Subtraction-----
+
+
+
+
+Enter a starting integer: 11
+============================= RESTART: F:\P1HW1.py =============================
+----- Calculating Exponenets-----
+
+
+
+
+Enter an integer as the base value:
+Enter an integer as the exponent:
+
+
+
+8 raised to the power of 4 is 4096 !!
+
+
+
+----- Addition and Subtraction-----
+
+
+
+
+Enter a starting integer: 11
+Enter an integer to add: 7
+Enter an integer to subtract: 5
+
+
+
+11 + 7 - 5 is equal to 13
+============================= RESTART: F:\P1HW1.py =============================
+----- Calculating Exponenets-----
+
+
+
+
+Enter an integer as the base value:8
+Enter an integer as the exponent:4
+
+
+
+8 raised to the power of 4 is 4096 !!
+
+
+
+----- Addition and Subtraction-----
+
+
+
+
+Enter a starting integer: 11
+Enter an integer to add: 7
+Enter an integer to subtract: 5
+
+
+
+11 + 7 - 5 is equal to 13
